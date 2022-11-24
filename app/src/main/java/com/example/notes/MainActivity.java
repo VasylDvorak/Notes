4e8b9fbc4ba3be29d22fd3d2257b1f19f694a9ad
Vasyl <dvorak1981@yandex.ru>
2022-11-25 02:19:42 +0300

    All types of menus were implemented: popup, toolbar, side drawer and context.

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -3,25 +3,44 @@ package com.example.notes;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        initToolbar();
+                initToolBar();
+
+
+        if(!isLandscape())
+            initToolbarAndDrawer();
+        else
+            initToolBar();
 
         if (savedInstanceState == null) getSupportFragmentManager()
                 .beginTransaction()
@@ -30,32 +49,106 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        menu.clear();
+            getMenuInflater().inflate(R.menu.menu, menu);
+            if (isLandscape()){
+                menu.add(Menu.NONE, 6, Menu.NONE, R.string.Send_note);
+                menu.add(Menu.NONE, 7, Menu.NONE, R.string.Add_photo);
+
+            }
         return super .onCreateOptionsMenu(menu);
     }
+
 
     @Override
     public boolean onOptionsItemSelected( @NonNull MenuItem item) {
         int id = item.getItemId();
+
         switch (id) {
-            case R.id.action_about:
-                createOneButtonAlertDialog();
+            case R.id.action_search:
+                // Сделать поиск
+                createOneButtonAlertDialog("Сделать поиск");
                 return true ;
-            case R.id.action_exit:
-                finish();
+            case R.id.action_sort:
+                createOneButtonAlertDialog("Сделать сортировку");
                 return true ;
+            case 6:
+                createOneButtonAlertDialog("Сделать переслать заметку");
+                return true ;
+            case 7:
+                createOneButtonAlertDialog("Сделать длобавить фото");
+                return true ;
+
         }
         return super .onOptionsItemSelected(item);
     }
 
-    private void initToolbar() {
+    private Toolbar initToolBar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
     }
 
-    private void createOneButtonAlertDialog() {
+    private void initToolbarAndDrawer(){
+        Toolbar toolbar = initToolBar();
+        initDrawer(toolbar);
+    }
+
+    private void initDrawer(Toolbar toolbar){
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+
+
+
+
+        View headerLayout = navigationView.inflateHeaderView(R.layout.drawer_header);
+        TextView headerTitle = (TextView)headerLayout.findViewById(R.id.header_title_id);
+        headerTitle.setText(R.string.header_title);
+        ShapeableImageView avatar= (ShapeableImageView)headerLayout.findViewById(R.id.header_avatar);
+        avatar.setImageResource(R.drawable.avatar);
+        headerTitle = (TextView)headerLayout.findViewById(R.id.header_title_id2);
+        headerTitle.setText(R.string.status);
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.action_about:
+                        createOneButtonAlertDialog("Программа блокнот");
+                        return true;
+                    case R.id.action_exit:
+
+                        drawerLayout.close();
+                        return true;
+                    case R.id.settings:
+                        createOneButtonAlertDialog("Сделать настройки");
+
+                        return true;}
+                return false;
+            }
+        });
+
+
+
+    }
+
+
+
+
+
+
+    private void createOneButtonAlertDialog(String title_window) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.Title)
+        builder.setTitle(title_window)
                 .setMessage(R.string.version)
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -74,7 +167,11 @@ public class MainActivity extends AppCompatActivity {
         b.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
         b.setBackgroundColor(getResources().getColor(R.color.color_time_date));
     }
+    private boolean isLandscape() {
+        return getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+    }
 
 
-
-}

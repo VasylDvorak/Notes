77b27e664e550fd7510bd5e380f1cba8f5ce6a79
Vasyl <dvorak1981@yandex.ru>
2022-11-25 02:19:42 +0300

    All types of menus were implemented: popup, toolbar, side drawer and context.

--- app/src/main/java/com/example/notes/NotesFragment.java
+++ app/src/main/java/com/example/notes/NotesFragment.java
@@ -1,16 +1,25 @@
 package com.example.notes;
 
+
+
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -48,6 +57,7 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_notes, container, false);
     }
 
@@ -55,18 +65,24 @@ public class NotesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+        FloatingActionButton fl_button= view.findViewById(R.id.btnAdd);
+
+        registerForContextMenu(fl_button);
+
         if (savedInstanceState != null) {
             note = savedInstanceState.getParcelable(SELECTED_NOTE);
         }
 
         initNotes(view.findViewById(R.id.data_container));
 
+
         if (isLandscape()) {
             showLandNoteDetails(note);
         }
         FloatingActionButton btnOne = view.findViewById(R.id.about);
         btnOne.setOnClickListener(v -> {
-            createOneButtonAlertDialog();
+            createOneButtonAlertDialog("О приложении");
         });
     }
 
@@ -97,6 +113,7 @@ public class NotesFragment extends Fragment {
             layoutView.addView(tv);
 
             final int index = i;
+            initPopupMenu(view, tv, index);
             tv.setOnClickListener(v -> {
                 if (previous_position != 0) {
                     TextView text_prev = layoutView.findViewById(previous_position);
@@ -121,6 +138,33 @@ public class NotesFragment extends Fragment {
         }
     }
 
+
+    private void initPopupMenu(View rootView, TextView view, int index){
+        view.setOnLongClickListener(v -> {
+            Activity activity = requireActivity();
+            PopupMenu popupMenu = new PopupMenu(activity,v );
+            activity.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    switch (menuItem.getItemId()){
+                        case R.id.action_popup_delete:
+                            createOneButtonAlertDialog("Реализовать Delete");
+                            return true;
+                        case R.id.action_popup_correct:
+                            createOneButtonAlertDialog("Сделать коррекция");
+                            return true;
+                    }
+                    return true;
+                }
+            });
+            popupMenu.show();
+            return true;
+        });
+    }
+
+
+
     private void showNoteDetails(Note note) {
         this.note = note;
         if (isLandscape()) {
@@ -136,7 +180,7 @@ public class NotesFragment extends Fragment {
                 requireActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction =
                 fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.notes_container, noteFragment);
+        fragmentTransaction.add(R.id.notes_container, noteFragment, "NOTE_FRAGMENT");
         fragmentTransaction.addToBackStack("");
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         fragmentTransaction.commit();
@@ -152,9 +196,53 @@ public class NotesFragment extends Fragment {
         fragmentTransaction.commit();
     }
 
-    private void createOneButtonAlertDialog() {
+
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater= getActivity().getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.add:
+                createOneButtonAlertDialog("Реализовать добавить");
+                break;
+            case R.id.sort:
+                createOneButtonAlertDialog("Реализовать сортировать");
+                break;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, @NonNull MenuInflater inflater) {
+
+            menu.clear();
+
+            inflater.inflate(R.menu.menu, menu);
+
+    }
+
+
+
+    private void createOneButtonAlertDialog(String title_window) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.Title)
+        builder.setTitle(title_window)
                 .setMessage(R.string.version)
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -173,4 +261,6 @@ public class NotesFragment extends Fragment {
         b.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
         b.setBackgroundColor(getResources().getColor(R.color.color_time_date));
     }
-}
+
+
+    }

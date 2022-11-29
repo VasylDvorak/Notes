7579a02e85ef8bea7ecd915b7634cc94fbad600f
Vasyl <dvorak1981@yandex.ru>
2022-11-29 18:23:35 +0300

    All tasks were done according to home work 9.

---
+++ app/src/main/java/com/example/notes/DialogHeaderDrawerFragment.java
@@ -0,0 +1,48 @@
+package com.example.notes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class DialogHeaderDrawerFragment extends DialogFragment {
+    private OnDialogListener dialogListener;
+
+    public static DialogHeaderDrawerFragment newInstance() {
+        return new DialogHeaderDrawerFragment();
+    }
+
+    // Установим слушатель диалога
+    public void setOnDialogListener(OnDialogListener dialogListener) {
+        this.dialogListener = dialogListener;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_drawer_header_text_dialog, container, false);
+// Запретим пользователю выходить без выбора
+        setCancelable(false);
+        view.findViewById(R.id.btn_name).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                if (dialogListener != null) dialogListener.onDialogName();
+            }
+        });
+        view.findViewById(R.id.btn_profession).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dismiss();
+                if (dialogListener != null) dialogListener.onDialogProfession();
+            }
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/notes/OnDialogListener.java
@@ -0,0 +1,11 @@
+package com.example.notes;
+
+import android.widget.TextView;
+
+public interface OnDialogListener {
+    void onDialogName();
+
+    void onDialogProfession();
+
+    String CorrectText(TextView textView);
+}
